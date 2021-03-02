d5f1290fcadc95b43ca0c89717a8cc1d2d1ed4e8
Takumi Kuwabara <t200c038@gunma-u.ac.jp>
2021-03-02 19:37:35 +0900

    gitignoreを作成

---
+++ taskapp/InputViewController.swift
@@ -0,0 +1,23 @@
+//
+//  InputViewController.swift
+//  taskapp
+//
+//  Created by tk on 2021/03/01.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+    
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -7,13 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.datasource = self
     }
 
+// データ数
+func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+}
 
+// 内容
+func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableView{}
+ // 再利用cell
+let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: IndexPath)
+
+ return cell
+}
+
+// 選択したとき
+func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    ferformSegue(withIdentifier: "cellSegue", sender: nil)
+}
+
+// 削除可能
+func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) ->　UITableVIewSell.EditingStyle {
+return .delete
+}
+
+// deleteメソッド
+func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle forRowAt indexPath: IndexPath) {
 }
 
